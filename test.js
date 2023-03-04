db0c529269ce73ee909cef03cb96d2e9d63f00bc
eddyboutros <eddy.boutros.36@gmail.com>
2023-03-04 13:58:48 +0200

    excel apis

---
+++ test.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+const excel = require('read-excel-file/node');
+app.use(cors());
+
+app.get('/getAvailableExcelFiles', function (req, res) {
+  const directoryPath = process.cwd();
+
+  fs.readdir(directoryPath, function(err, files) {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+
+    const excelFiles = files.filter(function(file) {
+      return path.extname(file).toLowerCase() === '.xlsx' || path.extname(file).toLowerCase() === '.xls';
+    });
+
+    const names = excelFiles.map(function(file) {
+      return { name: file };
+    });
+
+    res.send(names);
+  });
+});
+
+app.get('/readExcelData/:fileName', function (req, res) {
+  const fileName = req.params.fileName;
+  const directoryPath = process.cwd();
+
+  excel(directoryPath + "/" + fileName).then((rows) => {
+    res.send(rows);
+  }).catch((error) => {
+    res.send("File does not exist!");
+  });
+});
+
+app.listen(8080, function () {
+  console.log('Server is running on 8080');
+});
